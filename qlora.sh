30e57ba2601480fe0b9cef875f68f751107977e0
JackBosco <jackb@Boscos-PC.localdomain>
2024-04-15 17:09:16 -0400

    added fixes to qlora in script

--- qlora.sh
+++ qlora.sh
@@ -1,5 +1,8 @@
 #! /bin/bash
 
-git clone https://github.com/artidoro/qlora
+if [ ! -d "qlora" ]; then
+	git clone https://github.com/artidoro/qlora
+	cp ./_qlora.py qlora/_qlora.py
+fi
 cd qlora
-python3 qlora.py --dataset i../data.jsonl --trust_remote_code True 
+python3 _qlora.py --model_name_or_path microsoft/phi-2 --dataset i../data.jsonl --trust_remote_code True 
