87572163bf09d883b03ae11a05a5e02e5141fea5
JackBosco <jackb@Boscos-PC.localdomain>
2024-04-15 17:14:43 -0400

    makes qlora work

--- qlora.sh
+++ qlora.sh
@@ -5,4 +5,5 @@ if [ ! -d "qlora" ]; then
 	cp ./_qlora.py qlora/_qlora.py
 fi
 cd qlora
+git reset --hard 7f4e95a
 python3 _qlora.py --model_name_or_path microsoft/phi-2 --dataset i../data.jsonl --trust_remote_code True 
