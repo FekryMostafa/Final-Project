72736aee1f6a14fcb4bc57cd168d879734eec983
JackBosco <90462129+JackBosco@users.noreply.github.com>
2024-04-15 18:13:29 -0400

    added comments

--- qlora.sh
+++ qlora.sh
@@ -1,9 +1,10 @@
 #! /bin/bash
 
+# this literally pulls the whole qlora repo down
 if [ ! -d "qlora" ]; then
 	git clone https://github.com/artidoro/qlora
-	cp ./_qlora.py qlora/_qlora.py
+	cp ./_qlora.py qlora/_qlora.py # I changed ONE LINE to make things run
 fi
 cd qlora
-git reset --hard 7f4e95a
-python3 _qlora.py --model_name_or_path microsoft/phi-2 --dataset i../data.jsonl --trust_remote_code True 
+git reset --hard 7f4e95a # this is the last version we used
+python3 _qlora.py --model_name_or_path microsoft/phi-2 --dataset ../data.jsonl --trust_remote_code True 
